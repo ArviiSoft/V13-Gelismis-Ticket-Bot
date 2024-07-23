22e7a72f4d6caa44630f4dad70557e38a5eb0ca1
ArviS <alkannefee@gmail.com>
2024-07-23 23:10:41 +0330

    Add files via upload

---
+++ başlat.bat
@@ -0,0 +1,7 @@
+@echo off
+title arviis. - V13 Advanced Ticket System
+cls
+:arviis 
+node arvis.js
+goto arviis 
+pause
---
+++ test.bat
@@ -0,0 +1,7 @@
+@echo off
+title arviis. - V13 Advanced Ticket System [TEST]
+cls
+:arviis2 
+npm run arviis:dev
+goto arviis2 
+pause
